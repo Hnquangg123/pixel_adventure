d78a18de7b5e0851b7446c0a15935cf9998ce461
Kel <Hnquangg123@gmail.com>
2024-07-12 02:03:44 +0800

    Add Player + Animation

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -6,23 +6,27 @@ import 'package:flame/game.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
 class PixelAdventure extends FlameGame {
-
   @override
   Color backgroundColor() => const Color(0xFF211f30);
 
   late final CameraComponent cam;
 
-  final world = Level();
+  final world = Level(
+    levelName: 'Level-01',
+  );
 
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    // Load all images into cache
+    await images.loadAllImages();
 
-    cam = CameraComponent.withFixedResolution(width: 640, height: 360, world: world);
+    cam = CameraComponent.withFixedResolution(
+        width: 640, height: 360, world: world);
 
     cam.viewfinder.anchor = Anchor.topLeft;
 
     addAll([cam, world]);
+
     return super.onLoad();
   }
-  
 }
